dd893dee86f620eef40a4328100c3976e9b6497d
Nala <nalastorm@gmail.com>
2014-04-07 17:26:10 -0600

    First Commit

---
+++ main.js
@@ -0,0 +1,38 @@
+$(document).ready()
+
+// $(document).scroll(function(e){
+ 
+//     alert("you are scrolling!!")
+ 
+// });
+
+// var scrollAmount = $(window).scrollTop();
+// console.log(scrollAmount)
+
+// var documentHeight = $(document).height();
+// console.log(documentHeight)
+
+// var scrollPercent = (scrollAmount / 2049) * 100;
+// console.log(scrollPercent)
+
+// var start;
+
+var start_time = new Date();
+
+var end_time = new Date();
+
+var elapsed_ms = end_time - start_time;
+var seconds = Math.round(elapsed_ms / 1000);
+var minutes = Math.round(seconds / 60);
+var hours = Math.round(minutes / 60);
+
+var sec = TrimSecondsMinutes(seconds);
+var min = TrimSecondsMinutes(minutes);
+
+function TrimSecondsMinutes(elapsed) {
+    if (elapsed >= 60)
+        return TrimSecondsMinutes(elapsed - 60);
+    return elapsed;
+}
+
+console.log(trimSeconds`Minutes)
